3b4dd319fa98d63a7853c956a6c9d62bea5497e2
V0idHat <74353033+Parab-Mishra@users.noreply.github.com>
2025-05-02 17:44:23 +0000

    Initial project setup

---
+++ cmd/server/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Starting matchmaking engine...")
+    // TODO: Start router and initialize in-memory store
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Parab-Mishra/Matchmaking_Engine
+
+go 1.24.1
+
+require github.com/mmcloughlin/geohash v0.10.0 // indirect
